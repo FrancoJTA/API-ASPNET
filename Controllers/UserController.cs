22d8f8743f75f61a593e7e136222bbf41763e929
Franco <franco.torrez.alv@gmail.com>
2024-10-28 08:33:29 -0400

    api web

---
+++ Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using ApiWeb.Data;
+using ApiWeb.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiWeb.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : Controller
+{
+    private MyDBContext _mdb;
+
+    public UserController(MyDBContext mdb)
+    {
+        _mdb = mdb;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<User>>> Listar()
+    {
+        return await _mdb.Users.ToListAsync();
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult<User>> CreateUser([FromBody] User user)
+    {
+        if (user == null)
+        {
+            return BadRequest("User data is null.");
+        }
+
+        _mdb.Users.Add(user);
+        await _mdb.SaveChangesAsync();
+
+        // Retorna el usuario creado y un código de estado 201 Created
+        return CreatedAtAction(nameof(Listar), new { id = user.Id }, user);
+    }
+}
---
+++ Data/MyDBContext.cs
@@ -0,0 +1,10 @@
+﻿using ApiWeb.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiWeb.Data;
+
+public class MyDBContext : DbContext
+{
+    public MyDBContext(DbContextOptions<MyDBContext> options) : base(options) {}
+    public DbSet<User> Users { get; set; }
+}
